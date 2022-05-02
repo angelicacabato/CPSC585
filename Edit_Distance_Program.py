2c73194e9096738484ddb62dc67189cdc439ec8e
angelicacabato <58801573+angelicacabato@users.noreply.github.com>
2022-05-01 17:06:31 -0700

    Add files via upload
    Initial upload of Edit_Distance_Program.py

---
+++ Edit_Distance_Program.py
@@ -0,0 +1,180 @@
+# Angelica Cabato
+# Dr. Shawn X Wang, Professor
+# CPSC 485 - 01
+# 2 May 2022
+
+# Programming Assignment (due May 2):
+#
+# Write a program to calculate the edit distance between two words and output
+# both the matrix of distance calculation and an alignment that demonstrates
+# the result. Your program should ask for input of two words, calculate the
+# edit distance, output the matrix of calculation and an alignment that
+# verifies the edit distance. You may use any programming language that you
+# are most familiar. In front of your submission you should include a
+# paragraph of comments describing how I can compile and test your program,
+# including the required tools/environment. MATLAB is not acceptable since I
+# do not have MATLAB in my computer.
+#
+# The interface of the program looks like the following link.
+#
+# http://wang.ecs.fullerton.edu/cpsc485/editdist.html
+
+import numpy as np
+
+
+def print_alignment(alignment, word, i, j,word_alignment, diff, pos):
+    if pos == 2:
+        if i <= 0 and j <= 0:
+            print(word_alignment)
+            return word_alignment
+        if diff == 0:
+            if i <= 0 or j <= 0:
+                print(word_alignment)
+                return word_alignment
+        if alignment[i, j] == 'd':
+            idx = (i + diff) - 1
+            word_alignment = word[idx] + word_alignment
+            print_alignment(alignment, word, i - 1, j - 1, word_alignment,
+                            diff, pos)
+        else:
+            if alignment[i, j] == 'u':
+                idx = (i + diff) - 1
+                word_alignment = word[idx] + word_alignment
+                word_alignment = '_' + word_alignment
+                print_alignment(alignment, word, i - 1, j, word_alignment,
+                                diff, pos)
+            else:
+                idx = (i + diff) - 1
+                word_alignment = word[idx] + word_alignment
+                # word_alignment = '_' + word_alignment
+                print_alignment(alignment, word, i, j - 1, word_alignment, diff,
+                            pos)
+    else:
+        if i <= 0 and j <= 0:
+            print(word_alignment)
+            return word_alignment
+        if alignment[i, j] == 'd':
+            idx = (i + diff) - 1
+            word_alignment = word[idx] + word_alignment
+            print_alignment(alignment, word, i - 1, j - 1, word_alignment, diff,
+                            pos)
+        else:
+            if alignment[i, j] == 'u':
+                idx = (i + diff) - 1
+                word_alignment = word[idx] + word_alignment
+                # word_alignment = '_' + word_alignment
+                print_alignment(alignment, word, i - 1, j, word_alignment, diff,
+                            pos)
+            else:
+                # idx = (i + diff) - 1
+                # word_alignment = word[idx] + word_alignment
+                word_alignment = '_' + word_alignment
+                print_alignment(alignment, word, i, j - 1, word_alignment, diff,
+                            pos)
+
+
+def edit_distance(first_word, second_word):
+    indel = 1
+
+    n = len(first_word)
+    m = len(second_word)
+
+    row = n + 1
+    col = m + 1
+
+    # initializing the matrices
+    x = np.zeros((row, col), dtype=int)
+    alignment = np.empty((row, col), dtype=str)
+
+    if row > col:
+        larger_axis = row
+    else:
+        larger_axis = col
+
+    for idx in range(larger_axis):
+        if idx <= n:
+            x[idx, 0] = idx
+            alignment[idx, 0] = 'u'
+        if idx <= m:
+            x[0, idx] = idx
+            alignment[0, idx] = 'l'
+
+    alignment[0, 0] = 'd'
+
+    first_word_alignment = " "
+    second_word_alignment = " "
+
+    for i in range(row):
+        for j in range(col):
+            if i == 0 or j == 0:
+                continue
+            else:
+                cur_cell = x[i, j]
+                left_cell = x[i, j - 1]
+                up_cell = x[i - 1, j]
+                diag_cell = x[i - 1, j - 1]
+                a = first_word[i - 1]
+                b = second_word[j - 1]
+                if (first_word[i - 1] != second_word[j - 1]):
+                    closest_neighbors = (left_cell, up_cell, diag_cell)
+                    x[i, j] = min(closest_neighbors) + indel
+                    if (min(closest_neighbors) == left_cell):
+                        alignment[i, j] = 'l'
+                    if (min(closest_neighbors) == up_cell):
+                        alignment[i, j] = 'u'
+                    elif (min(closest_neighbors) == diag_cell):
+                        alignment[i, j] = 'd'
+                else:
+                    closest_neighbors = (left_cell, up_cell, diag_cell)
+                    x[i, j] = min(closest_neighbors)
+                    if (min(closest_neighbors) == left_cell):
+                        alignment[i, j] = 'l'
+                    if (min(closest_neighbors) == up_cell):
+                        alignment[i, j] = 'u'
+                    elif (min(closest_neighbors) == diag_cell):
+                        alignment[i, j] = 'd'
+
+
+    # Print out information
+    print("The matrix:")
+    print(x)
+    print("The edit distance is: ", x[n, m])
+
+    print(alignment)
+    # print("i =", i)
+    # print("j =", j)
+
+    # Print Alignment
+    first_word_alignment = " "
+    second_word_alignment = " "
+
+    if n != m:
+        first_word_alignment = print_alignment(alignment, first_word, i, j,
+                                               first_word_alignment, diff=0,pos=1)
+        second_word_alignment = print_alignment(alignment, second_word, i, j,second_word_alignment, diff=1, pos=2)
+    else:
+        first_word_alignment = print_alignment(alignment, first_word, i, j, first_word_alignment, diff=0,pos=1)
+        second_word_alignment = print_alignment(alignment, second_word, i, j,
+                                                second_word_alignment, diff=0, pos=2)
+
+
+def main():
+    #first_word = "evaluation"
+    # second_word = "revolution"
+
+    first_word = "EXECUTION"
+    second_word = "INTENTION"
+
+    # Accept User Input
+    # TODO: Input Validation
+    # first_word = input("Enter first word: ")
+    # print("First word is: " + first_word)
+
+    #  second_word = input("Enter second word:")
+    # print("Second word is: " + second_word)
+
+    edit_distance(first_word, second_word)
+
+
+if __name__ == "__main__":
+    main()
